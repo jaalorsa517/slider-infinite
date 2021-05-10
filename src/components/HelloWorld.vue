2a54d40ff26a2d6ac1b0ba8e46c1c1d7aa34786c
Jaime <jaalorsa517@hotmail.com>
2021-05-09 21:29:23 -0500

    proyecto para un slider inifinte con css

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,56 +1,61 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+<template lang="pug">
+  .carousel
+    .container
+      .container-img(v-for="pokemon,index of pokemons" :key="index")
+        img.img(:src="pokemon.img" :alt="pokemon.name" )
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  export default {
+    name: 'HelloWorld',
+    data: function() {
+      return {
+        pokemons: [],
+      };
+    },
+    created: function() {
+      const uri = 'https://pokeapi.co/api/v2/pokemon?limit=10';
+      fetch(uri)
+        .then(response => response.json())
+        .then(json => {
+          Promise.all(json.results.map(pokemon => fetch(pokemon.url))).then(resp =>
+            Promise.all(resp.map(r => r.json())).then(pokemons => {
+              pokemons.forEach(pokemon => {
+                this.pokemons.push({
+                  name: pokemon.name,
+                  img: pokemon.sprites.other['official-artwork']['front_default'],
+                });
+              });
+              this.pokemons.forEach(pokemon => this.pokemons.push(pokemon));
+            })
+          );
+        });
+    },
+  };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="stylus">
-h3
-  margin 40px 0 0
+<style lang="stylus">
+  @keyframes movie {
+     0%{transform:translateX(0)}
+     100%{transform:translateX(calc(-240px * 10))}
+   }
+  .carousel
+    width 100%
+    height 240px
+    position relative
+    margin auto
+    overflow hidden
 
-ul
-  list-style-type none
-  padding 0
+  .container
+    animation movie 10s linear infinite
+    width calc(240px * 20)
+    display flex
 
-li
-  display inline-block
-  margin 0 10px
+    .container-img
+      width 240px
+      height 240px
 
-a
-  color #42b983
+      .img
+        height 240px
+        width 240px
 </style>
